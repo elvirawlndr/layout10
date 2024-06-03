095ba234452e87e33486923f4b2793b8527c2579
Elvira Wulandari <ewulandari2804@gmail.com>
2024-06-03 23:07:35 +0700

    update tutorial 13 - networking part 2

--- lib/tutorial_12.dart
+++ lib/tutorial_12.dart
@@ -3,6 +3,41 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+class Product {
+  final int id;
+  final String name;
+  final int price;
+
+  const Product({
+    required this.id,
+    required this.name,
+    required this.price,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      price: json['price'] ?? 0,
+    );
+  }
+}
+
+void main() {
+  runApp(const Tutorial12App());
+}
+
+class Tutorial12App extends StatelessWidget {
+  const Tutorial12App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Tutorial12(),
+    );
+  }
+}
+
 class Tutorial12 extends StatefulWidget {
   const Tutorial12({super.key});
 
@@ -13,17 +48,19 @@ class Tutorial12 extends StatefulWidget {
 class _Tutorial12State extends State<Tutorial12> {
   List<dynamic> products = []; // List untuk menampung data produk
 
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+
   // Fungsi untuk mengambil data dari API
   Future<void> fetchData() async {
   try {
-    var response = await http.get(Uri.parse('http://192.168.1.45:8000/api/product'));
+    var response = await http.get(Uri.parse('http://192.168.1.34:8000/api/product'));
 
     if (response.statusCode == 200) {
       var responseData = json.decode(response.body);
       print(response.body);
       if (responseData != null && responseData['list'] != null) {
         setState(() {
-          // Mengambil data produk dari kunci 'products'
           products = responseData['list'];
         });
       } else {
@@ -35,8 +72,27 @@ class _Tutorial12State extends State<Tutorial12> {
   } catch (e) {
     print('Error fetching data: $e');
   }
-
 }
+
+  Future<void> addProduct(String name, int price) async {
+    try {
+      var response = await http.post(
+        Uri.parse('http://192.168.1.34:8000/api/product'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'name': name, 'price': price}),
+      );
+      print(response.statusCode);
+      print(response.body);
+
+      if (response.statusCode == 200) {
+        fetchData();
+      } else {
+        throw Exception('Failed to add product');
+      }
+    } catch (e) {
+      print('Error adding product: $e');
+    }
+  }
 
 
   @override
@@ -57,9 +113,54 @@ class _Tutorial12State extends State<Tutorial12> {
           return ListTile(
             title: Text(products[index]['name']),
             subtitle: Text(products[index]['price'].toString()),
-            // Tambahkan aksi lainnya sesuai kebutuhan
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context, 
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Add Product'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(labelText: 'Name'),
+                    ),
+                    TextField(
+                      controller: priceController,
+                      decoration: const InputDecoration(labelText: 'Price'),
+                      keyboardType: TextInputType.number,
+                    )
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop;
+                    }, 
+                    child: const Text('Cancel'),
+                  ),
+                    TextButton(
+                      onPressed: () {
+                        final String name = nameController.text;
+                        final int price = int.parse(priceController.text);
+
+                        addProduct(name, price);
+
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Save'),
+                    )
+                  ]
+                );
+            }
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
